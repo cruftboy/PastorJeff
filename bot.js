f8900d15c39b06cd34128ea203175dcd8b65c898
cruftboy <jasperhkw@gmail.com>
2017-10-19 14:01:05 -0700

    add some shit, remove other shit

--- bot.js
+++ bot.js
@@ -1,22 +1,10 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(logger.transports.Console, {
-    colorize: true
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
 });
-logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
-});
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
+
 bot.on('message', function (user, userID, channelID, message, evt) {
     if (message.toLowerCase().indexOf("heck") >= 0 || message.toLowerCase().indexOf("fuck") >= 0 || message.toLowerCase().indexOf("shit") >= 0 || message.toLowerCase().indexOf("darn") >= 0 || message.toLowerCase().indexOf("nut") >= 0 || message.toLowerCase().indexOf("anus") >= 0 || message.toLowerCase().indexOf("gosh") >= 0 || message.toLowerCase().indexOf("ass") >= 0 || message.toLowerCase().indexOf("bum") >= 0 || message.toLowerCase().indexOf("butt") >= 0 || message.toLowerCase().indexOf("cunt") >= 0 || message.toLowerCase().indexOf("bitch") >= 0 || message.toLowerCase().indexOf("bastard") >= 0 || message.toLowerCase().indexOf("fart") >= 0 || message.toLowerCase().indexOf("frick") >= 0) {
         bot.sendMessage({
@@ -40,3 +28,5 @@ bot.on('message', function (user, userID, channelID, message, evt) {
         });
     }
 });
+
+client.login(process.env.BOT_TOKEN);
